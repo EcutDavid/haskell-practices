0dfeebe10b3f7c678959e714a5185fefba321a78
David Guan <davidguandev@gmail.com>
2018-01-28 16:29:06 +1100

    Quick sort

--- 01-sorting/sort.hs
+++ 01-sorting/sort.hs
@@ -1,4 +1,4 @@
-insertSort :: (Ord a) => [a] -> [a]
+-- insertSort :: (Ord a) => [a] -> [a]
 insertSort [] = []
 insertSort (firEle:listLeft) = insert firEle (insertSort listLeft)
     where insert a [] = [a]
@@ -6,6 +6,15 @@ insertSort (firEle:listLeft) = insert firEle (insertSort listLeft)
             | a < b = a : b : c
             | otherwise = b : insert a c
 
+
+quickSort :: (Ord a) => [a] -> [a]
+quickSort [] = []
+quickSort [x] = [x]
+quickSort (x:listLeft) = smallerOnes ++ [x] ++ largerOnes
+    where smallerOnes = quickSort [ele | ele <- listLeft, ele <= x]
+          largerOnes = quickSort [ele | ele <- listLeft, ele > x]
+
 main :: IO()
 main = do
-  print (insertSort [1,23,123,12,312,31,23,12,1])
+  print (insertSort [1,23,123,12,312,31,23,12,1,12,312,312,3])
+  print (quickSort [1,23,123,12,312,31,23,12,1,12,312,312,3])
